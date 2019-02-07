017db4cf54d4bd0bec637b575f007aed8d9cc9a4
James Askew <jamesaskew@outlook.com>
2019-02-07 20:28:34 +0000

    Add calculation class to wrap all services

---
+++ Courier.Api/PostageCalculation.cs
@@ -0,0 +1,48 @@
+﻿using Courier.Domain.Models;
+using Courier.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courier.Api
+{
+    public class PostageCalculation
+    {
+        private readonly ISizeService _sizeService;
+        private readonly IPriceService _priceService;
+        private readonly IOrderService _orderService;
+
+        public PostageCalculation(ISizeService sizeService, IPriceService priceService, IOrderService orderService)
+        {
+            this._sizeService = sizeService;
+            this._priceService = priceService;
+            this._orderService = orderService;
+        }
+
+        public Order CalculateOrder(List<Parcel> parcels)
+        {
+            var prices = new List<Price>();
+
+            foreach (var parcel in parcels)
+            {
+                var size = this._sizeService.GetParcelSize(parcel);
+                var price = this._priceService.GetPrice(size);
+
+                prices.Add(new Price
+                {
+                    Size = size,
+                    Amount = price
+                });
+            }
+
+            var order = new Order
+            {
+                Prices = prices,
+                TotalCost = this._orderService.GetTotalPriceForOrder(prices)
+            };
+
+            return order;
+        }
+    }
+}
---
+++ Courier.Domain/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Courier.Domain.Models
+{
+    public class Order
+    {
+        public IEnumerable<Price> Prices { get; set; }
+        public int TotalCost { get; set; }
+    }
+}
