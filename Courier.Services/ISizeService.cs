5dc4e3723062184aed15791ac753262b6d7a6738
James Askew <jamesaskew@outlook.com>
2019-02-07 19:55:22 +0000

    Added tests to get parcel size

---
+++ Courier.Services/ISizeService.cs
@@ -0,0 +1,13 @@
+﻿using Courier.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Courier.Domain.Models;
+
+namespace Courier.Services
+{
+    public interface ISizeService
+    {
+        ParcelSize GetParcelSize(Parcel parcel);
+    }
+}
---
+++ Courier.Services/SizeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Courier.Domain.Constants;
+using Courier.Domain.Enums;
+using Courier.Domain.Models;
+
+namespace Courier.Services
+{
+    public class SizeService : ISizeService
+    {
+        public ParcelSize GetParcelSize(Parcel parcel)
+        {
+            return ParcelSize.Small;            
+        }
+    }
+}
