f9cabdf0ab2a95a8a635a4d0303c82c397012d36
James Askew <jamesaskew@outlook.com>
2019-02-07 20:08:15 +0000

    complete GetPrice method

--- Courier.Services/PriceService.cs
+++ Courier.Services/PriceService.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Courier.Domain.Enums;
-using Courier.Domain.Models;
 
 namespace Courier.Services
 {
@@ -11,9 +7,26 @@ namespace Courier.Services
     {
         public int GetPrice(ParcelSize parcelSize)
         {
-            return 0;
+            int price;
+            switch (parcelSize)
+            {
+                case ParcelSize.Small:
+                    price = 300;
+                    break;
+                case ParcelSize.Medium:
+                    price = 800;
+                    break;
+                case ParcelSize.Large:
+                    price = 1500;
+                    break;
+                case ParcelSize.ExtraLarge:
+                    price = 2500;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(parcelSize), parcelSize, null);
+            }
+
+            return price;
         }
-
-
     }
 }
