91e3c55723f9edbbdbaab4093f20ced56ea1654f
James Askew <jamesaskew@outlook.com>
2019-02-07 19:59:55 +0000

    Added tests for parcel price

---
+++ Courier.Services/PriceService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Courier.Domain.Enums;
+using Courier.Domain.Models;
+
+namespace Courier.Services
+{
+    public class PriceService : IPriceService
+    {
+        public int GetPrice(ParcelSize parcelSize)
+        {
+            return 0;
+        }
+
+
+    }
+}
